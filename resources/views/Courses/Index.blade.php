d51ac6ff8090b56a3b080a23d9cf41278db77fd8
unknown <a6519150007@utch.edu.mx>
2021-08-29 16:26:56 -0600

    adding a space

--- resources/views/Courses/Index.blade.php
+++ resources/views/Courses/Index.blade.php
@@ -7,6 +7,7 @@
 <ul>
     <h1>Welcome</h1><br>
     <a href=" {{route('courses.create')}}">Create course</a><br>
+    
     @foreach ($courses as $course)
     <li>
         <a href="{{route('courses.show',$course->id)}}">{{$course->name}}</a>
