aad0c1bdbd23e75703c554d7726535586eff4d2a
unknown <a6519150007@utch.edu.mx>
2021-08-29 14:15:22 -0600

    searching by id in show

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CursoController extends Controller
 {
     public function index(){
-        return view('Courses.Index');
+
+        //getting the courses from the db use all or paginate to get pages
+        $courses =Course::paginate();
+        //calling to the view and sending the data we got from de db through the variable courses
+        return view('Courses.Index', compact('courses'));
     }
     public function create(){
         return view('Courses.Create');
     }
-    public function show($var){
-        return view('Courses.Show', compact('var'));
+    public function show($id){
+        $course = Course::find($id);
+        return view('Courses.Show', compact('course'));
         //compact returns: ['var' => $var]
     }
 }
