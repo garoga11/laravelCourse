aeb5be68065a80d0127ea187521e7e7443e8a28e
unknown <a6519150007@utch.edu.mx>
2021-08-29 16:26:26 -0600

    adding edit option

--- resources/views/Courses/Show.blade.php
+++ resources/views/Courses/Show.blade.php
@@ -7,7 +7,8 @@
 <h1>Show {{$course->name}} </h1>
 
 <a href=" {{route('courses.index')}}">Go back</a>
-
+<br>
+<a href=" {{route('courses.edit', $course)}}">Edit course</a>
 <p><strong>Category: </strong> {{$course->category}} </p>
 
 <p>{{$course->description}}</p>
