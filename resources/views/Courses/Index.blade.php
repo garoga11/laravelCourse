3acafdf8619bfa9d2cd306fc9de519a64bd4c03a
unknown <a6519150007@utch.edu.mx>
2021-08-29 14:15:50 -0600

    showing the course data

--- resources/views/Courses/Index.blade.php
+++ resources/views/Courses/Index.blade.php
@@ -4,6 +4,18 @@
 
 @section('content')
 
-<h1>Index</h1>
+<ul>
+    <h1>Welcome</h1><br>
+    <a href=" {{route('courses.create')}}">Create course</a><br>
+    @foreach ($courses as $course)
+    <li>
+        <a href="{{route('courses.show',$course->id)}}">{{$course->name}}</a>
+        
+    </li>
+        
+    @endforeach
+</ul>
+
+{{$courses->links()}}
 
 @endsection
