38e06ba8562f41bf68677b4f3f38b5d68c279beb
unknown <a6519150007@utch.edu.mx>
2021-08-29 16:27:25 -0600

    creating the form to create a course

--- resources/views/Courses/Create.blade.php
+++ resources/views/Courses/Create.blade.php
@@ -5,5 +5,27 @@
 @section('content')
 
 <h1>Create</h1>
+<form action="{{route('courses.store')}}" method="POST">
+
+    @csrf
+    <label>
+        Nombre:<br>
+        <input type="text" name="name"><br>
+    </label>
+
+    <label >
+        Description:<br>
+        <textarea name="description"  rows="5"></textarea><br>
+    </label>
+
+    <label >
+        Category:<br>
+        <input type="text" name="category"><br>
+    </label>
+
+    <br>
+
+    <button type="submit">Submit</button>
+</form>
 
 @endsection
