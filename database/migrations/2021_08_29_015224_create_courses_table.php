fbf42384ad4701e76c5e6aabdda499b589a4d012
unknown <a6519150007@utch.edu.mx>
2021-08-28 21:30:56 -0600

    creating courses table

--- database/migrations/2021_08_29_015224_create_courses_table.php
+++ database/migrations/2021_08_29_015224_create_courses_table.php
@@ -17,6 +17,7 @@ class CreateCoursesTable extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
+            $table->text('category');
             $table->timestamps();
         });
     }
