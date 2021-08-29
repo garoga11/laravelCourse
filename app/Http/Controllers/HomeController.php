8a2f5347a8e88b6f7a844e0d11937f1fb50eb135
unknown <a6519150007@utch.edu.mx>
2021-08-28 18:06:44 -0600

    creting the function invoke

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    //Invoke se usa cuando el controllor solo tiene una ruta
+    public function __invoke(){
+        return "WELCOME";
+    }
 }
