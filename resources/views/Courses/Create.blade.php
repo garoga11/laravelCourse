ac68ab018e60bbaa2473aa179ffc06a151aed24a
garoga11 <garoga11>
2021-08-30 13:30:08 -0600

    valitation of empty form in the view

--- resources/views/Courses/Create.blade.php
+++ resources/views/Courses/Create.blade.php
@@ -12,17 +12,31 @@
         Nombre:<br>
         <input type="text" name="name"><br>
     </label>
+    @error('name')
+        <br>
+        <small>*{{$message}}</small>
+        <br>
+    @enderror
 
     <label >
         Description:<br>
         <textarea name="description"  rows="5"></textarea><br>
     </label>
+    @error('description')
+        <br>
+        <small>*{{$message}}</small>
+        <br>
+    @enderror
 
     <label >
         Category:<br>
         <input type="text" name="category"><br>
     </label>
-
+    @error('category')
+    <br>
+    <small>*{{$message}}</small>
+    <br>
+@enderror
     <br>
 
     <button type="submit">Submit</button>
