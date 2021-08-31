63d2586c20da0606de15b46a76e02e1719dc404c
garoga11 <garoga11>
2021-08-31 12:21:16 -0600

    creating the cont view

---
+++ resources/views/contactanos/index.blade.php
@@ -0,0 +1,51 @@
+@extends('Layouts.plantilla')
+
+@section('title', 'Contact')
+
+@section('content')
+
+<h1>Dejanos un mensaje</h1>
+<form action="{{route('contactanos.store')}}" method="POST">
+
+@csrf
+
+    <label>
+        Nombre:
+        <br>
+        <input type="text" name="name">
+    </label>
+    <br>
+    @error('name')
+    <p><strong>{{$message}}</strong></p>
+@enderror
+
+    <label>
+        Correo
+        <br>
+        <input type="text" name="correo">
+    </label>
+    <br>
+    @error('correo')
+    <p><strong>{{$message}}</strong></p>
+@enderror
+
+    <label>
+        Mensaje:
+        <br>
+        <textarea name="mensaje" rows="4"></textarea>
+    </label>
+    @error('mensaje')
+    <p><strong>{{$message}}</strong></p>
+@enderror
+
+    <button type="submit">Submit</button>
+</form>
+
+@if (session('info'))
+<script>
+    alert("{{session('info')}}");
+</script>
+    
+@endif
+
+@endsection
